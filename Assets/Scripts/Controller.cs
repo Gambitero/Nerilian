92d1757b18101c5d888011c4a1b8a48fa13a3de7
Rodron <alexsp2399@gmail.com>
2021-03-04 21:03:40 +0100

    Co-authored-by: Nestor Hernandez Benito <n.hernandezb.2017@alumnos.urjc.es>

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,6 +10,11 @@ public class Controller : MonoBehaviour
     private float jumpHeight = 2.4f;
 
     public bool flagCamera = false;
+    public bool flagTurn = false;
+
+    public int turnDir = 0;
+    public float angle = 270f;
+    public float rotationDir = -0.25f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -19,6 +24,29 @@ public class Controller : MonoBehaviour
 
     public bool isGrounded;
     
+
+    void OnTriggerEnter(Collider obj){
+        if (obj.gameObject.CompareTag("Turnpoint")){
+            flagTurn = true;
+            gameObject.transform.parent = obj.gameObject.transform;
+            Debug.Log("papi chulo");
+        }
+    }
+
+    void OnTriggerExit(Collider obj){
+        if (obj.gameObject.CompareTag("Turnpoint")){
+            flagTurn = false;
+            gameObject.transform.parent = null;
+            Debug.Log("papi chulo adios");
+        }
+    }
+
+    float standardAngle(float angle){
+        if(angle<0)
+            return 360f+angle;
+        return angle;
+    }
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -43,18 +71,42 @@ public class Controller : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
 
-        if(Input.GetButtonUp("CameraRotation"))
+        if(Input.GetButtonUp("CameraRotation") && flagTurn)
         {
             flagCamera = true;
+
+            switch(turnDir){
+                case 0:
+                    angle = 0f; //norte
+                    rotationDir = -1;
+                    turnDir = 1;
+                    break;
+                case 1:
+                    angle = 90f; //oeste
+                    rotationDir = 1;
+                    turnDir = 0;
+                    break;
+                case 2:
+                    angle = 180f; //este
+                    rotationDir = -1;
+                    turnDir = 3;
+                    break;
+                case 3:
+                    angle = 270f; //sur
+                    rotationDir = 1;
+                    turnDir = 0;
+                    break;
+
+            }
         }
 
         if (flagCamera)
         {
             //Time.deltaTime * -50f
-            controller.gameObject.transform.Rotate(0f, -.25f, 0f);            
-            if (controller.gameObject.transform.rotation.eulerAngles.y <= 270f){
-                flagCamera = false;
+            controller.gameObject.transform.Rotate(0f, 0.25f * rotationDir, 0f);            
+            if (controller.gameObject.transform.rotation.eulerAngles.y <= standardAngle(angle + 90f) * rotationDir){
+                flagCamera = false;                
             }
         }
-    }    
+    }
 }
