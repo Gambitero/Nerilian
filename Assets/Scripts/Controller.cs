41debbb29666045c240280dda5a71c61bf0349f1
Rodron <alexsp2399@gmail.com>
2021-03-14 18:30:36 +0100

    Giros de cámara hechos

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +8,19 @@ public class Controller : MonoBehaviour
     private float speed = 12f;
     private float gravity = -9.81f*9;
     private float jumpHeight = 2.4f;
+    
+    private float currentTurn;
+    private Collider turnObj = null;
+    private int turnDir = 0;
+    private float turnProgress = 0f;
 
     public bool flagCamera = false;
-    public bool flagTurn = false;
-
-    public int turnDir = 0;
-    public float angle = 270f;
+    public bool flagTurn = false;    
+    
+    public float angle;
     public float rotationDir = -0.25f;
+
+    public Vector3 newPos;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -24,20 +30,28 @@ public class Controller : MonoBehaviour
 
     public bool isGrounded;
     
+    void SetTurnValues(){        
+        turnDir = turnObj.gameObject.GetComponent<Turnpoint>().turnDir;
+        turnObj.gameObject.GetComponent<Turnpoint>().turnDir *= -1;
+        newPos = turnObj.gameObject.transform.position;
+
+        turnProgress = 0f;
+        
+        Debug.Log("TurnDir ==> " + turnDir);
+    }
 
     void OnTriggerEnter(Collider obj){
         if (obj.gameObject.CompareTag("Turnpoint")){
             flagTurn = true;
-            gameObject.transform.parent = obj.gameObject.transform;
-            Debug.Log("papi chulo");
+            Debug.Log("Enter turnpoint");
+            turnObj = obj;            
         }
     }
 
-    void OnTriggerExit(Collider obj){
+    void OnTriggerExit(Collider obj){        
         if (obj.gameObject.CompareTag("Turnpoint")){
             flagTurn = false;
-            gameObject.transform.parent = null;
-            Debug.Log("papi chulo adios");
+            Debug.Log("Exit turnpoint");
         }
     }
 
@@ -47,8 +61,39 @@ public class Controller : MonoBehaviour
         return angle;
     }
 
+    void TurnPlayer(){
+        turnProgress += 0.25f;
+
+        controller.gameObject.transform.Rotate(0f, 0.25f * turnDir, 0f);
+
+        if (turnProgress + 0.001f >= 90){
+            Debug.Log("Stop");
+            controller.gameObject.transform.eulerAngles = new Vector3(controller.gameObject.transform.rotation.eulerAngles.x, angle + 90 * turnDir, controller.gameObject.transform.rotation.eulerAngles.z);
+            flagCamera = false;
+            return;
+        }
+    }
+
     void Update()
     {
+        if (flagCamera)
+        {
+            /*
+            //Time.deltaTime * -50f
+            controller.gameObject.transform.Rotate(0f, 0.25f * rotationDir, 0f);
+            Debug.Log("---");
+            Debug.Log(controller.gameObject.transform.rotation.eulerAngles.y);
+            
+            if (controller.gameObject.transform.rotation.eulerAngles.y <= 8007f0as7f0){
+                controller.gameObject.transform.eulerAngles = new Vector3(controller.gameObject.transform.rotation.eulerAngles.x, 8007f0as7f0, controller.gameObject.transform.rotation.eulerAngles.z);
+                //controller.gameObject.transform.rotation.eulerAngles.Set(controller.gameObject.transform.rotation.eulerAngles.x, 8007f0as7f0-12f, controller.gameObject.transform.rotation.eulerAngles.z);
+                flagCamera = false;                
+            }
+            */
+            TurnPlayer();
+            return;
+        }
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
@@ -73,40 +118,23 @@ public class Controller : MonoBehaviour
 
         if(Input.GetButtonUp("CameraRotation") && flagTurn)
         {
-            flagCamera = true;
+            SetTurnValues();
+            this.gameObject.transform.position = new Vector3(newPos.x, this.gameObject.transform.position.y, newPos.z);
+            
+            angle = controller.gameObject.transform.rotation.eulerAngles.y;
+            
+            Debug.Log("The Starting Angle");
+            Debug.Log(angle);
 
-            switch(turnDir){
-                case 0:
-                    angle = 0f; //norte
-                    rotationDir = -1;
-                    turnDir = 1;
-                    break;
-                case 1:
-                    angle = 90f; //oeste
-                    rotationDir = 1;
-                    turnDir = 0;
-                    break;
-                case 2:
-                    angle = 180f; //este
-                    rotationDir = -1;
-                    turnDir = 3;
-                    break;
-                case 3:
-                    angle = 270f; //sur
-                    rotationDir = 1;
-                    turnDir = 0;
-                    break;
+            angle = standardAngle(angle + 0.25f * turnDir) + 0.25f * -turnDir;
 
-            }
-        }
+            Debug.Log("The Fixed Starting Angle");
+            Debug.Log(angle);
 
-        if (flagCamera)
-        {
-            //Time.deltaTime * -50f
-            controller.gameObject.transform.Rotate(0f, 0.25f * rotationDir, 0f);            
-            if (controller.gameObject.transform.rotation.eulerAngles.y <= standardAngle(angle + 90f * rotationDir)){
-                flagCamera = false;                
-            }
-        }
+            flagCamera = true;            
+            
+            Debug.Log("The Angle");
+            Debug.Log(angle + 90 * turnDir);
+        }        
     }
 }
