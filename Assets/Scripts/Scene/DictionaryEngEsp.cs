7d92f25d856fd77571164219c7ab1494981d3ef6
nesherben <nestor8299@gmail.com>
2021-07-03 03:02:54 +0200

    diccionario

--- Assets/Scripts/Scene/DictionaryEngEsp.cs
+++ Assets/Scripts/Scene/DictionaryEngEsp.cs
@@ -9,10 +9,12 @@ public class DictionaryEngEsp : MonoBehaviour
     public GameObject SettingControl;
     List<string> textos = new List<string>{"VOLVER", "EMPEZAR", "JUGAR", "OPCIONES", "VOLUMEN", "IDIOMA",
                         "MUNDO 1", "MUNDO 2", "MUNDO 3", "CLASE", "CONTINUAR", "GUERRERO", "PICARO", "MAGO", "NORMAL",
-                         "PODER", "DASH", "NADA", "SALTO DOBLE", "GUARDAR", "SALIR","GENERAL","MUSICA","EFECTOS", "SALIR AL MENU"};
+                         "PODER", "DASH", "NADA", "SALTO DOBLE", "GUARDAR", "SALIR","GENERAL","MUSICA","EFECTOS", "SALIR AL MENU",
+                        "NIVEL","PUNTOS OBTENIDOS:"};
     List<string> texts = new List<string>{"BACK", "START", "PLAY", "OPTIONS", "VOLUME", "LANGUAGE",
                         "WORLD 1", "WORLD 2", "WORLD 3", "CLASS", "CONTINUE", "WARRIOR", "ROGUE", "WIZARD", "NORMAL",
-                         "POWER", "DASH", "NOTHING", "DOUBLE JUMP", "SAVE", "EXIT", "GENERAL", "MUSIC", "EFFECTS", "EXIT TO MENU" };
+                         "POWER", "DASH", "NOTHING", "DOUBLE JUMP", "SAVE", "EXIT", "GENERAL", "MUSIC", "EFFECTS", "EXIT TO MENU",
+                        "LEVEL", "OBTAINED POINTS"};
     private void Update()
     {
         if (LanguageSystem.languageValue == 0) { 
