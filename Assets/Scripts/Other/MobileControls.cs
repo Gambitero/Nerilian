6fe38768bf57659e1719a0a0a2dbdcb590f7e369
Rodron <alexsp2399@gmail.com>
2021-07-04 08:12:33 +0200

    Build ready

--- Assets/Scripts/Other/MobileControls.cs
+++ Assets/Scripts/Other/MobileControls.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class MobileControls : MonoBehaviour
 {    
     void Start()
-    {        
+    {
+        /*
         if (!MobileChecker.isMobile()){
             gameObject.SetActive(false);
-        }
+        }*/
     }
 }
